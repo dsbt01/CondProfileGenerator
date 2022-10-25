3ca64e0568968f311bfb9e5ab619e07b1a55586d
David Suarez <dsuarez001@att.net>
2022-10-24 21:00:58 -0400

    fixed

--- src/js/app.js
+++ src/js/app.js
@@ -93,10 +93,10 @@ function render(variables = {}) {
           <h2>${occupation}</h2>
           <h3>${city}, ${country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="{"https://twitter.com/" + ((variables.twitter) ? variables.twitter : "4geeksacademy")}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="{"https://github.com/" + ((variables.github) ? variables.github : "4geeksacademy")}"><i class="fab fa-github"></i></a></li>
+            <li><a href="{"https://linkedin.com" + ((variables.linkedin) ? variables.linkedin : "4geeksacademy")}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="{"https://instagram.com" + ((variables.instagram) ? variables.instagram : "4geeksacademy")}"><i class="fab fa-instagram"></i></a></li>
           </ul>
           <h3>${twitterString}</h3>
           <h3>${githubString}</h3>
